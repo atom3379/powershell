714b7c63cc8087d0ac4bda04b1fc07439a04f290
PWT <93472862+atom3379@users.noreply.github.com>
2025-05-01 14:27:29 +0700

    Create notify.ps1

---
+++ notify.ps1
@@ -0,0 +1,1 @@
+{Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Hello world', 'This is called from a batch script.', [System.Windows.Forms.ToolTipIcon]::None)}
