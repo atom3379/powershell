4b3e816400627ec405631cb54159f7a65aa8ebc2
PWT <93472862+atom3379@users.noreply.github.com>
2025-05-07 13:51:21 +0700

    Update multitool.bat

--- multitool.bat
+++ multitool.bat
@@ -21,10 +21,9 @@ echo 3. Open a Program
 echo 4. Shutdown Computer
 echo 5. Restart Computer
 echo 6. Enter BIOS
-echo 7. Access Password File
-echo 8. Open WSL (Windows Subsystem for Linux)
-echo 9. Exit
-echo 10. Go to GUI Mode(close this window)
+echo 7. Open WSL (Windows Subsystem for Linux)
+echo 8. Exit
+echo 9. Go to GUI Mode(close this window)
 echo ========================================================================================================================
 set /p choice="Choose an option (1-10): "
 
@@ -34,10 +33,9 @@ set /p choice="Choose an option (1-10): "
     if "%choice%"=="4" goto shutdown
     if "%choice%"=="5" goto restart
     if "%choice%"=="6" goto bios
-    if "%choice%"=="7" goto passwords
-    if "%choice%"=="8" goto wsl
-    if "%choice%"=="9" exit
-    if "%choice%"=="10" goto guimode
+    if "%choice%"=="7" goto wsl
+    if "%choice%"=="8" exit
+    if "%choice%"=="9" goto guimode
 
 :chrome
 echo Opening Google Chrome...
@@ -74,42 +72,6 @@ shutdown /r /fw /t 0
 exit
 
 
-:: Initialize attempt counter
-set attempts=0
-
-:passwordloop
-cls
-echo ==============================
-echo       ACCESS PASSWORD FILE
-echo ==============================
-echo Attempts remaining: %attempts% / 10
-set /p pass="Enter the password: "
-
-
-:: Check password
-color 0a
-if "%pass%"=="NeverGonnaGiveYouUp" goto openfile
-
-:: Incorrect password
-color 0a
-set /a attempts+=1
-if %attempts% geq 10 (
-    echo Too many incorrect attempts. Returning to the menu...
-    timeout /t 3 >nul
-    goto menu
-)
-
-echo Incorrect password! Access denied.
-timeout /t 2 >nul
-goto passwordloop
-
-:openfile
-echo Access granted! Opening password file...
-notepad "C:\Users\UsEr\OneDrive\Documents\New folder\New folder\New folder\epicgamespassword.txt"
-echo Returning to the menu...
-timeout /t 2 >nul
-goto menu
-
 :wsl
 echo Opening WSL in a new window...
 start wsl
@@ -118,5 +80,5 @@ timeout /t 2 >nul
 goto menu
 
 :guimode
-start powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Users\UsEr\OneDrive\เดสก์ท็อป\multi-tool.ps1"
+powershell -Command "% iwr https://raw.githubusercontent.com/atom3379/powershell/refs/heads/main/multitool.ps1 | iex"
 exit
